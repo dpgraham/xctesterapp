ee94fb92e6adabc04ac291922616df9bedece4b2
dpgraham <dan@saucelabs.com>
2020-04-28 23:45:08 -0700

    Initial Commit

---
+++ XCTesterApp/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  XCTesterApp
+//
+//  Created by Daniel-Graham-SL1298 on 4/28/20.
+//  Copyright © 2020 Daniel-Graham-SL1298. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
